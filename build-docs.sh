d11983ef43d0d0fa159d1b659d9d3c090ffaf09d
Callum Macpherson <callum.macpherson@quantinuum.com>
2026-04-08 16:58:52 +0100

    more fixes

--- build-docs.sh
+++ build-docs.sh
@@ -9,12 +9,9 @@ cp pytket-docs-theming/conf.py .
 # Run link checker in C.I.
 sphinx-build -b linkcheck . build -W
 
-set +e
-
 # Build pages
 sphinx-build -b html . build -W -D html_title="pytket user guide"
 
-# Remove copied files. This ensures reusability.
-rm -r _static 
-rm -r quantinuum-sphinx
+set +e
+
 rm conf.py
